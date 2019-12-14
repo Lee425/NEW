abfb459bb751f2d664eb19c49540aa61b0ee59f6
Lee425 <31365773+Lee425@users.noreply.github.com>
2019-12-14 21:37:21 +0800

    Update train_test.py

--- code/train_test.py
+++ code/train_test.py
@@ -3,24 +3,27 @@ import numpy as np
 class NEW:
     def __init__(self, args):
         self.args = args
+        self.lr = args.learning_rate
+        self.lam = args.lam
+        self.epsilon = args.epsilon
         self.theta = None
 
-    def computerCost(self, X, y, lam, theta):
+    def computerCost(self, X, y, theta):
         m = y.shape[0]
         C = X.dot(theta) - y
-        J_cost = np.sqrt(((C.T.dot(C)) + lam * (theta.T.dot(theta))) / (2 * m))
+        J_cost = np.sqrt(((C.T.dot(C)) + self.lam * (theta.T.dot(theta))) / (2 * m))
         return J_cost
 
-    def gradientDescent(self, X, y, lam, theta, alpha, epsilon):
+    def gradientDescent(self, X, y, theta):
         J_history = []
         iter = 0
         error = 0
         lt = []
         while True and iter < 1e6:
-            J_cost = self.computerCost(X, y, lam, theta)
-            theta = theta * (1 - alpha * lam) - (alpha * (X.T.dot(X.dot(theta) - y)))
+            J_cost = self.computerCost(X, y, theta)
+            theta = theta * (1 - self.lam * self.lr) - (self.lr * (X.T.dot(X.dot(theta) - y)))
             J_history.append(J_cost)
-            if np.abs(J_cost - error) < epsilon:
+            if np.abs(J_cost - error) < self.epsilon:
                 break
             else:
                 error = J_cost
@@ -36,7 +39,7 @@ class NEW:
         x = train_x[:, :-1] ** self.args.k
         x = np.column_stack((bias, x))
         w = train_x[:, -1]
-        J_history, theta, lt = self.gradientDescent(x, w, self.args.lam, init_theta, self.args.learning_rate, self.args.epsilon)
+        J_history, theta, lt = self.gradientDescent(x, w, init_theta)
         self.theta = theta
         # print("theta:",self.theta)
 
